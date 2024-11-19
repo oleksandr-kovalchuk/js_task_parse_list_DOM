5e30fa72170c22c9647ae4ec66735a0df53fd767
Oleksandr Kovalchuk <kov280396@gmail.com>
2024-11-19 16:16:19 +0200

    small improvements

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,30 +1,27 @@
 'use strict';
 
 const dataList = [...document.querySelectorAll('li')];
+const ul = document.querySelector('ul');
 
 function stringToNum(string) {
   return Number(string.replace('$', '').replace(',', ''));
 }
 
 function sortList(list) {
-  list.sort((a, b) => {
-    return stringToNum(b.dataset.salary) - stringToNum(a.dataset.salary);
-  });
-
-  const ul = document.querySelector('ul');
+  list.sort(
+    (a, b) => stringToNum(b.dataset.salary) - stringToNum(a.dataset.salary),
+  );
 
   list.forEach((el) => ul.appendChild(el));
 }
 
 function getEmployees(list) {
-  return list.map((el) => {
-    return {
-      name: el.textContent,
-      position: el.dataset.position,
-      salary: el.dataset.salary,
-      age: el.dataset.age,
-    };
-  });
+  return list.map((el) => ({
+    name: el.textContent,
+    position: el.dataset.position,
+    salary: el.dataset.salary,
+    age: el.dataset.age,
+  }));
 }
 
 sortList(dataList);
